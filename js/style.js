2eba5653da23a68d370dc66cd90e2e3571a75ce5
mohamed tamaa <mohamed.tamaa@smartcomput.net>
2021-01-18 18:23:58 +0200

    video

--- js/style.js
+++ js/style.js
@@ -1,6 +1,13 @@
-$(".add").click(function(){
-    $(".main-page-one").removeClass("hidden");
-  });
-$(".add").click(function(){
+$(".work").click(function(){
     $(".main-page").addClass("hidden");
+    $(".work i").addClass("active");
+    $(".add i").removeClass("active");
+    $(".work-examples").removeClass("hidden");
+  });
+
+$(".add").click(function(){
+    $(".main-page").removeClass("hidden");
+    $(".work i").removeClass("active");
+    $(".add i").addClass("active");
+    $(".work-examples").addClass("hidden");
   });
