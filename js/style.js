40f3a98c43331d5179963deacc82fec9bd6474b5
mohamed tamaa <mohamed.tamaa@smartcomput.net>
2021-01-22 20:09:54 +0200

    final 1

--- js/style.js
+++ js/style.js
@@ -1,17 +1,50 @@
+$(".me").click(function(){
+    $(".main-page").removeClass("hidden");
+    $(".me i").addClass("active");
+    $(".work i").removeClass("active");
+    $(".cv-cv i").removeClass("active");
+    $(".contact-c i").removeClass("active");
+    $(".work-examples").addClass("hidden");
+    $(".cv").addClass("hidden");
+    $(".contact").addClass("hidden");
+  });
+
 $(".work").click(function(){
     $(".main-page").addClass("hidden");
+    $(".cv").addClass("hidden");
+    $(".contact").addClass("hidden");
     $(".work i").addClass("active");
-    $(".add i").removeClass("active");
+    $(".me i").removeClass("active");
+    $(".cv-cv i").removeClass("active");
+    $(".contact-c i").removeClass("active");
     $(".work-examples").removeClass("hidden");
   });
 
-$(".add").click(function(){
-    $(".main-page").removeClass("hidden");
-    $(".work i").removeClass("active");
-    $(".add i").addClass("active");
+$(".cv-cv").click(function(){
+    $(".main-page").addClass("hidden");
     $(".work-examples").addClass("hidden");
+    $(".contact").addClass("hidden");
+    $(".cv-cv i").addClass("active");
+    $(".work i").removeClass("active");
+    $(".me i").removeClass("active");
+    $(".contact-c i").removeClass("active");
+    $(".cv").removeClass("hidden");
+  });
+
+$(".contact-c").click(function(){
+    $(".main-page").addClass("hidden");
+    $(".work-examples").addClass("hidden");
+    $(".cv").addClass("hidden");
+    $(".contact-c i").addClass("active");
+    $(".me i").removeClass("active");
+    $(".work i").removeClass("active");
+    $(".cv-cv i").removeClass("active");
+    $(".contact").removeClass("hidden");
   });
 
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+
+//data-wow-duration="2s"
